5397e535ce88d8b11d4f11da71cf7fb968cbf18b
Leftmyhand <159633841+AdityaIza@users.noreply.github.com>
2024-03-15 22:39:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/restaurantreview/data/retrofit/ApiService.kt
@@ -0,0 +1,46 @@
+package com.example.restaurantreview.data.retrofit
+
+import com.example.restaurantreview.data.response.PostReviewResponse
+import com.example.restaurantreview.data.response.RestaurantResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
+}
+
+class ApiConfig {
+    companion object{
+        fun getApiService(): ApiService {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://restaurant-api.dicoding.dev/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+
+
+        }
+    }
+}
